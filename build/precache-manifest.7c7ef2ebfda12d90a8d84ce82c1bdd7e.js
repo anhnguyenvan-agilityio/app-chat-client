904bccd186937a44e3b336353cb1077957436ce8
anhnguyenvan <anh.nguyenvan@asnet.com.vn>
2019-06-03 17:38:26 +0700

    init structure

---
+++ build/precache-manifest.7c7ef2ebfda12d90a8d84ce82c1bdd7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201eba5964c047d5659c76ec78f2a8b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f17f59bd7369cde191ce",
+    "url": "/static/css/main.bf9eaee5.chunk.css"
+  },
+  {
+    "revision": "8a5ab34837c5648bb427",
+    "url": "/static/js/2.ee951d30.chunk.js"
+  },
+  {
+    "revision": "f17f59bd7369cde191ce",
+    "url": "/static/js/main.f6baada8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
