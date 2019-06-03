11d2b58baae7f4cbb2ced48c7f332cacf217e708
anhnguyenvan <anh.nguyenvan@asnet.com.vn>
2019-06-03 17:57:06 +0700

    Updates

---
+++ precache-manifest.6627a61addf3a460dccd1d02b760cd8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8839d5921ce30756c3416125c8c8b6a",
+    "url": "/chat-app-client/index.html"
+  },
+  {
+    "revision": "f17f59bd7369cde191ce",
+    "url": "/chat-app-client/static/css/main.bf9eaee5.chunk.css"
+  },
+  {
+    "revision": "8a5ab34837c5648bb427",
+    "url": "/chat-app-client/static/js/2.ee951d30.chunk.js"
+  },
+  {
+    "revision": "f17f59bd7369cde191ce",
+    "url": "/chat-app-client/static/js/main.f6baada8.chunk.js"
+  },
+  {
+    "revision": "705f8e475359daf92217",
+    "url": "/chat-app-client/static/js/runtime~main.fbc7452e.js"
+  }
+]);
